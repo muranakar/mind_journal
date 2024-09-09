a54864ec145195cdb5062b0ba9dac27d027bdd98
[muranakar] <[P1211065@gmail.com]>
2024-09-10 06:11:14 +0900

    fix テストコード一部酒精

--- test/diary_database_test.dart
+++ test/diary_database_test.dart
@@ -17,7 +17,7 @@ void main() {
 
     // テスト用のデータベースを初期化
     database = DiaryDatabase.instance;
-    
+    await database!.testDatabase;
   });
 
   tearDown(() async {
@@ -26,82 +26,86 @@ void main() {
     await deleteDatabase(path);
   });
 
-  // test('CRUD処理を行えているか', () async {
-  //   // Create
-  //   final diary = Diary(
-  //       title: 'Test Diary',
-  //       content: 'This is a test diary.',
-  //       isFavorite: false,
-  //       createdAt: DateTime.now(),
-  //       updatedAt: DateTime.now(),
-  //       emotionImage: 'happy.png',
-  //       tags: ['Flutter', 'Test']);
+  test('CRUD処理を行えているか', () async {
+    // Create
+    final diary = Diary(
+        title: 'Test Diary',
+        content: 'This is a test diary.',
+        isFavorite: false,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        emotionImage: 'happy.png',
+        tags: ['Flutter', 'Test']);
 
-  //   await database.createDiary(diary);
-  //   expect(diary.id, isNotNull);
+    await database.createDiary(diary);
+    expect(diary.id, isNotNull);
 
-    // Read
-  //   List<Diary> diaries = await database.readAllDiaries();
-  //   expect(diaries.length, 1);
-  //   expect(diaries.first.title, 'Test Diary');
+    
+    List<Diary> diaries = await database.readAllDiaries();
+    expect(diaries.length, 1);
+    expect(diaries.first.title, 'Test Diary');
 
-  //   // Update
-  //   diary.title = 'Updated Diary';
-  //   await database.updateDiary(diary);
+    // Update
+    diary.title = 'Updated Diary';
+    await database.updateDiary(diary);
 
-  //   final updatedDiary = await database.readAllDiaries();
-  //   expect(updatedDiary.first.title, 'Updated Diary');
+    final updatedDiary = await database.readAllDiaries();
+    expect(updatedDiary.first.title, 'Updated Diary');
 
-  //   // Delete
-  //   await database.deleteDiary(diary.id!);
-  //   diaries = await database.readAllDiaries();
-  //   expect(diaries.isEmpty, true);
-  //       // Clean up
-  //   await database.deleteDiary(diary.id!);
-  // });
+    // Delete
+    await database.deleteDiary(diary.id!);
+    diaries = await database.readAllDiaries();
+    expect(diaries.isEmpty, true);
+        // Clean up
+    await database.deleteDiary(diary.id!);
+  });
 
-  // test('全てのタグを取得し、使用頻度順に並んでいるかどうか', () async {
-  //   // Create a new diary with tags
-  //   final diary = Diary(
-  //       title: 'Tag Test Diary 1',
-  //       content: 'This diary is for testing tags.',
-  //       isFavorite: true,
-  //       createdAt: DateTime.now(),
-  //       updatedAt: DateTime.now(),
-  //       emotionImage: 'neutral.png',
-  //       tags: ['Flutter', 'Dart', 'Database']);
+  test('全てのタグを取得し、使用頻度順に並んでいるかどうか', () async {
+    // Create a new diary with tags
+    final diary = Diary(
+        title: 'Tag Test Diary 1',
+        content: 'This diary is for testing tags.',
+        isFavorite: true,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        emotionImage: 'neutral.png',
+        tags: ['Flutter', 'Dart', 'Database']);
 
-  //   final diary2 = Diary(
-  //       title: 'Tag Test Diary 2',
-  //       content: 'This diary is for testing tags.',
-  //       isFavorite: true,
-  //       createdAt: DateTime.now(),
-  //       updatedAt: DateTime.now(),
-  //       emotionImage: 'neutral.png',
-  //       tags: ['Flutter', 'Database']);
+    final diary2 = Diary(
+        title: 'Tag Test Diary 2',
+        content: 'This diary is for testing tags.',
+        isFavorite: true,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        emotionImage: 'neutral.png',
+        tags: ['Flutter', 'Database']);
 
-  //   final diary3 = Diary(
-  //       title: 'Tag Test Diary 3',
-  //       content: 'This diary is for testing tags.',
-  //       isFavorite: true,
-  //       createdAt: DateTime.now(),
-  //       updatedAt: DateTime.now(),
-  //       emotionImage: 'neutral.png',
-  //       tags: ['Flutter']);
+    final diary3 = Diary(
+        title: 'Tag Test Diary 3',
+        content: 'This diary is for testing tags.',
+        isFavorite: true,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        emotionImage: 'neutral.png',
+        tags: ['Flutter']);
 
-  //   await database.createDiary(diary);
-  //   await database.createDiary(diary2);
-  //   await database.createDiary(diary3);
+    await DiaryDatabase.instance.createDiary(diary);
+    await DiaryDatabase.instance.createDiary(diary2);
+    await DiaryDatabase.instance.createDiary(diary3);
 
-  //   // Get all tags sorted by usage
-  //   final tags = await database.fetchAllTagsSortedByUsage();
+    // Get all tags sorted by usage
+    List<String> tags = await database.fetchAllTagsSortedByUsage();
+    print(tags);
+    // 最初の三要素だけを取得
+    tags = tags.sublist(0, 3);
 
-  //   // Check that tags are in the correct order
-  //   expect(tags,matcher.equals(['Flutter', 'Database', 'Dart']));
+  
+    // Check that tags are in the correct order
+    expect(tags,matcher.equals(['Flutter', 'Database', 'Dart']));
 
-  //   // Clean up
-  //   await database.deleteDiary(diary.id!);
-  //   await database.deleteDiary(diary2.id!);
-  //   await database.deleteDiary(diary3.id!);
-  // });
+    // Clean up
+    await database.deleteDiary(diary.id!);
+    await database.deleteDiary(diary2.id!);
+    await database.deleteDiary(diary3.id!);
+  });
 }
